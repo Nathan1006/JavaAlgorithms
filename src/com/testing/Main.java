d7204303a60c5c727e24453b1cf16b0d532d0a11
Nathan1006 <nrpetit@outlook.com>
2022-01-27 17:46:47 -0500

    Hackerrank balanced brackets challenge completed on 1/27/2022

--- src/com/testing/Main.java
+++ src/com/testing/Main.java
@@ -1,11 +1,11 @@
 package com.testing;
 
 import com.testing.November22.HowManyDigits;
+import com.testing.Others.BalancedBrackets;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        HowManyDigits.numOfDigits(0);
     }
 }
---
+++ src/com/testing/Others/BalancedBrackets.java
@@ -0,0 +1,51 @@
+package com.testing.Others;
+
+import java.util.Stack;
+
+public class BalancedBrackets {
+    public static String isBalanced(String s) {
+        // Write your code here
+        Stack<Character> brackets = new Stack();
+
+        for(int i = 0; i < s.length(); i++){
+            switch(s.charAt(i)) {
+                case '(':
+                case '{':
+                case '[':
+                    brackets.push(s.charAt(i));
+                    break;
+            }
+            if (brackets.isEmpty()){
+                return "NO";
+            }
+            switch (s.charAt(i)) {
+                case ')':
+                    if (brackets.peek() == '('){
+                        brackets.pop();
+                    } else {
+                        return "NO";
+                    }
+                    break;
+                case '}':
+                    if (brackets.peek() == '{'){
+                        brackets.pop();
+                    } else {
+                        return "NO";
+                    }
+                    break;
+                case ']':
+                    if (brackets.peek() == '['){
+                        brackets.pop();
+                    } else {
+                        return "NO";
+                    }
+                    break;
+            }
+        }
+        if (brackets.isEmpty()) {
+            System.out.println("yes");
+            return "YES";
+        }
+        return "NO";
+    }
+}
