2b3678b8b848d25b0b6310fea9a6357bc6e9bccd
Nathan1006 <nrpetit@outlook.com>
2022-02-08 17:55:27 -0500

    Added in organizedballs algorithm; near complete

---
+++ src/com/testing/HackerRank/Others/ContainerOfBalls.java
@@ -0,0 +1,55 @@
+package com.testing.HackerRank.Others;
+
+import com.testing.November9.LargestDigit;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class ContainerOfBalls {
+    public static String organizingContainers(List<List<Integer>> container) {
+        // Write your code here
+        HashMap<Integer, Integer> numOfBalls = new HashMap<Integer, Integer>(); //total number of each type/color of ball
+        List<Integer> ContainerCapacities = new ArrayList<>();
+
+        System.out.println(container);
+
+        for (int row = 0; row < container.size(); row++) {
+            int capacity = 0;
+            for (int col = 0; col < container.get(row).size(); col++) {
+                int ballCount = container.get(row).get(col);
+                capacity += container.get(row).get(col);
+                if (numOfBalls.containsKey(col)){
+                    numOfBalls.put(col, row + ballCount);
+                } else {
+                    numOfBalls.put(col, row);
+                }
+            }
+            ContainerCapacities.add(capacity);
+        }
+
+
+        System.out.println(ContainerCapacities);
+        System.out.println(numOfBalls);
+
+        return "POSSIBLE";
+    }
+
+    public static void main(String[] args) {
+        List<List<Integer>> Container = new ArrayList<List<Integer>>();
+        List<Integer> balls = new ArrayList<Integer>();
+
+        balls.add(1);
+        balls.add(1);
+        Container.add(balls); // add first list
+        balls = new ArrayList<Integer>(balls); // create a new inner list that has the same content as
+        // the original inner list
+        balls.set(0, 2);
+        balls.set(1, 0);
+        Container.add(balls); // add second list
+
+        //container.get(0).add(300); // changes only the first inner list
+
+        System.out.println(organizingContainers(Container));
+    }
+}
--- src/com/testing/HackerRank/ThirtyDaysofCode/Day0.java
+++ src/com/testing/HackerRank/ThirtyDaysofCode/Day0.java
@@ -1,4 +1,13 @@
 package com.testing.HackerRank.ThirtyDaysofCode;
 
+import java.util.Scanner;
+
 public class Day0 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String inputString = sc.nextLine();
+        System.out.println("Hello, World.");
+        System.out.println(inputString);
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
 }
