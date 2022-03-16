7bcf8ce7de8b137453dedca51680787373b2187b
Nathan1006 <nrpetit@outlook.com>
2022-03-15 20:06:24 -0400

    Did day 10, need to add in day 1 - 9

--- src/com/testing/HackerRank/Others/ClimbingLeaderboard.java
+++ src/com/testing/HackerRank/Others/ClimbingLeaderboard.java
@@ -2,6 +2,7 @@ package com.testing.HackerRank.Others;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class ClimbingLeaderboard {
     public static List<Integer> climbingLeaderboard(List<Integer> ranked, List<Integer> player) {
@@ -11,29 +12,26 @@ public class ClimbingLeaderboard {
         for (Integer score : player) {
             int rank = 1;
             for (int i = 1; i < ranked.size(); i++) {
-                if (score < ranked.get(ranked.size() - 1)) {
-                    rank = ranked.size() - 1;
-                    break;
-                } else if (score < ranked.get(i - 1) && score > ranked.get(i)) {
-                    rank = i - 1;
-                    break;
-                } else if (score < ranked.get(i)) {
-                    rank = i + 1;
-                } else if (score.equals(ranked.get(i))) {
-                    rank = i;
-                    break;
-                }
+                if (score < ranked.get(i)) {
 
+                }
+                rank++;
+                if (ranked.get(i) == ranked.get(i-1)) {
+                    rank--;
+                }
             }
             newRanks.add(rank);
         }
+
+
+
 
         return newRanks;
     }
 
     public static void main(String[] args) {
 
-        List<Integer> ranked = List.of(100, 100, 50, 40, 40, 20,10);
+        List<Integer> ranked = List.of(100, 100, 50, 40, 40, 20, 10);
         List<Integer> player = List.of(5, 25, 50, 120);
 
         System.out.println(climbingLeaderboard(ranked, player));
--- src/com/testing/HackerRank/ThirtyDaysofCode/Day10.java
+++ src/com/testing/HackerRank/ThirtyDaysofCode/Day10.java
@@ -1,4 +1,30 @@
 package com.testing.HackerRank.ThirtyDaysofCode;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Day10 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+        String binary = Integer.toBinaryString(n);
+        int count = 0;
+        int largestCount = 0;
+
+        for (char num : binary.toCharArray()) {
+            count++;
+            if (num == '0') {
+                count = 0;
+            } else if (count > largestCount) {
+                largestCount = count;
+            }
+        }
+
+        System.out.println(largestCount);
+
+        bufferedReader.close();
+    }
 }
+
